b6d2dd41e63b4e64991c5f9ab2f0aad07ea0535b
capuzzo-susanna <capuzzo.susanna@iisgalvanimi.edu.it>
2019-12-05 09:59:13 +0000

    esercizio

--- Angular/src/app/user-list/user-list.component.ts
+++ Angular/src/app/user-list/user-list.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-user-list',
-  templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  selector: ['app-user-list'];
+  templateUrl: ['./user-list.component.html'];
+  styleUrls: ['./user-list.component.css'];
 })
 export class UserListComponent implements OnInit {
+  
   names: string[];
+}
 
   constructor() {
-    this.names['pippo', 'pluto', 'paperino'];
+    this.names = ['pippo', 'pluto', 'paperino'];
   }
+  
+ 
 
-  }
 
-}
+  
+  
+
